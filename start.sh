2d1d1e369d34cf7df9b87523fbceb7c56b93ccbf
Jackie Chen <cms88168@users.noreply.github.com>
2015-11-30 02:11:42 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+echo "========================================================================"
+echo " You can now connect to this ShadowSocksR server:"
+echo " password: $SS_PASSWORD  protocol: $SS_PROTOCOL  obfs: $SS_OBFS "
+echo " timeout: $SS_TIMEOUT  encryption method: $SS_METHOD "
+echo " Please remember the password!"
+echo "========================================================================"
+
+cd shadowsocks/shadowsocks
+python server.py -c /etc/shadowsocksr.json
