20839326180713a73a9a27f795b33c0dfa6b86f1
Jackie Chen <cms88168@users.noreply.github.com>
2015-11-30 16:08:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,5 @@ echo " Please remember the password!"
 echo "========================================================================"
 
 cd ~/shadowsocks/shadowsocks
-echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"fast_open": false,"workers": 1}'
 echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"fast_open": false,"workers": 1}'>shadowsocksr.json
 python server.py -c shadowsocksr.json
