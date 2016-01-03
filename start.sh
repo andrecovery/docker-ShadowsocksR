3352936fb9cbc92835f59772d05d525cfa38abf8
Jackie Chen <cms88168@users.noreply.github.com>
2016-01-03 18:46:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 echo "========================================================================"
-echo " You can now connect to this ShadowSocksR server:"
-echo " password: $SS_PASSWORD  protocol: $SS_PROTOCOL  obfs: $SS_OBFS , $SS_OBFSP "
-echo " port: $SS_SERVER_PORT  timeout: $SS_TIMEOUT  encryption method: $SS_METHOD "
-echo " Please remember the password!"
+echo " You can now connect to this ShadowSocksR server at port: $SS_SERVER_PORT "
+echo " timeout: $SS_TIMEOUT  protocol: $SS_PROTOCOL , $SS_PROTOCOLP  obfs: $SS_OBFS , $SS_OBFSP "
+echo " encryption method: $SS_METHOD  redirect: $SS_REDIRECT  dnsv6: $SS_DNSIPV6 "
+echo " Please remember the password: $SS_PASSWORD "
 echo "========================================================================"
 
 cd ~/shadowsocks/shadowsocks
-echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"fast_open": false,"workers": 1}'>shadowsocksr.json
+echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","protocol_param":'$SS_PROTOCOLP',"obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"redirect":'$SS_REDIRECT',"dns_ipv6":"'$SS_DNSIPV6'","fast_open": false,"workers": 1}'>shadowsocksr.json
 python server.py -c shadowsocksr.json
