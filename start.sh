d915cc16aacce821b925b4d8b56c83d6b9fcbc54
Jackie Chen <cms88168@users.noreply.github.com>
2016-01-12 02:24:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,10 +19,8 @@ if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
 fi
 
 if [ ! -f /.root_pw_set ]; then
-	source /set_root_pw.sh
+	/set_root_pw.sh
 fi
-
-/usr/sbin/sshd -D
 
 echo "========================================================================"
 echo " You can now connect to this ShadowSocksR server at port: $SS_SERVER_PORT "
@@ -31,8 +29,7 @@ echo " encryption method: $SS_METHOD  redirect: $SS_REDIRECT  dnsv6: $SS_DNSIPV6
 echo " Please remember the password: $SS_PASSWORD "
 echo "========================================================================"
 
-cd ~/vpnserver
-source vpnserver start
-cd ~/shadowsocks/shadowsocks
 echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","protocol_param":'$SS_PROTOCOLP',"obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"redirect":'$SS_REDIRECT',"dns_ipv6":'$SS_DNSIPV6',"fast_open": false,"workers": 1}'>shadowsocksr.json
-python server.py -c shadowsocksr.json
+
+cd ~/shadowsocks/shadowsocks
+/usr/sbin/sshd -D && ~/vpnserver/./vpnserver start && python server.py -c shadowsocksr.json
