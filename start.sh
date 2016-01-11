6d1135bc6088171325c4c2cf2d5f5c4cbc705646
Jackie Chen <cms88168@users.noreply.github.com>
2016-01-12 03:08:14 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,5 +31,6 @@ echo "========================================================================"
 
 echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","protocol_param":'$SS_PROTOCOLP',"obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"redirect":'$SS_REDIRECT',"dns_ipv6":'$SS_DNSIPV6',"fast_open": false,"workers": 1}'>/shadowsocksr.json
 
-su -c "/usr/sbin/sshd -D;~/vpnserver/./vpnserver start;python ~/shadowsocks/shadowsocks/server.py -c /shadowsocksr.json"
-exit 0
+(sleep 2;/usr/sbin/sshd -D)&
+(sleep 1;python ~/shadowsocks/shadowsocks/server.py -c /shadowsocksr.json)&
+~/vpnserver/./vpnserver start
