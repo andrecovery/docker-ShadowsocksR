2172db68a82c3ea3d96f429a1b24254effe90321
Jackie Chen <cms88168@users.noreply.github.com>
2016-01-12 02:54:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ echo " encryption method: $SS_METHOD  redirect: $SS_REDIRECT  dnsv6: $SS_DNSIPV6
 echo " Please remember the password: $SS_PASSWORD "
 echo "========================================================================"
 
-echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","protocol_param":'$SS_PROTOCOLP',"obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"redirect":'$SS_REDIRECT',"dns_ipv6":'$SS_DNSIPV6',"fast_open": false,"workers": 1}'>shadowsocksr.json
+echo '{"server":"0.0.0.0","server_ipv6":"::","server_port":'$SS_SERVER_PORT',"local_address":"127.0.0.1","local_port":1080,"password":"'$SS_PASSWORD'","timeout":'$SS_TIMEOUT',"method":"'$SS_METHOD'","protocol":"'$SS_PROTOCOL'","protocol_param":'$SS_PROTOCOLP',"obfs":"'$SS_OBFS'","obfs_param":'$SS_OBFSP',"redirect":'$SS_REDIRECT',"dns_ipv6":'$SS_DNSIPV6',"fast_open": false,"workers": 1}'>/shadowsocksr.json
 
-cd ~/shadowsocks/shadowsocks
-(/usr/sbin/sshd -D;~/vpnserver/./vpnserver start;python server.py -c shadowsocksr.json)
+su -c "/usr/sbin/sshd -D;~/vpnserver/./vpnserver start;python ~/shadowsocks/shadowsocks/server.py -c /shadowsocksr.json"
+exit 0
